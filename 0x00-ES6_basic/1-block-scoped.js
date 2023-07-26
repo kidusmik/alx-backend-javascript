0c9aeaaaf582cb99f0635bf0f52a4f158a840ab8
kidusmik <kidusmik@gmail.com>
2023-07-26 17:35:35 +0000

    modify solution of task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
@@ -6,5 +7,6 @@ export default function taskBlock(trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+
   return [task, task2];
 }
