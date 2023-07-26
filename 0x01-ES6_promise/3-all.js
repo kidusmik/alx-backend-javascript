7f3913165def3aaeefae501bb3aea55696329907
kidusmik <kidusmik@gmail.com>
2023-07-26 18:43:13 +0000

    add solution for task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable comma-dangle */
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      console.log(
+        `${values[0].body} ${values[1].firstName} ${values[1].lastName}`
+      );
+    })
+    .catch(console.log('Signup system offline'));
+}
